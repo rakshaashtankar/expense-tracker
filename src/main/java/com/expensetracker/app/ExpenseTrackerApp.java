acd29eeaffcb674048175ea4cd635a2ca9e18a39
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2026-05-30 00:07:27 +0530

    Improve app structure and responsibility separation

--- src/main/java/com/expensetracker/app/ExpenseTrackerApp.java
+++ src/main/java/com/expensetracker/app/ExpenseTrackerApp.java
@@ -5,14 +5,98 @@ import com.expensetracker.model.Expense;
 import com.expensetracker.service.ExpenseService;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class ExpenseTrackerApp {
-    public static void main(String[] args) {
-        final ExpenseService expenseService = new ExpenseService();
-        Scanner sc = new Scanner(System.in);
+    private final ExpenseService expenseService;
+    private final Scanner sc;
+
+
+    public ExpenseTrackerApp() {
+        this.expenseService = new ExpenseService();
+        this.sc  = new Scanner(System.in);
+    }
+
+
+
+    public Expense readExpenseInput() {
+        System.out.println("\nEnter expense details");
+        System.out.println("Enter Amount");
+        double amount = sc.nextDouble();
+        sc.nextLine();
+        boolean isValidCategory = false;
+        String category = null;
+        while(!isValidCategory) {
+            System.out.println("Select from Category");
+            for (Category c: Category.values()) {
+                System.out.println(c);
+            }
+            category=sc.nextLine();
+            for (Category c: Category.values()) {
+                if(c.name().equalsIgnoreCase(category)) {
+                    isValidCategory = true;
+                    break;
+                }
+            }
+        }
+        System.out.println("Enter Date(YYYY-MM-DD)");
+        LocalDate date = LocalDate.parse(sc.nextLine());
+        System.out.println("Enter Description");
+        String description = sc.nextLine();
+        return new Expense(amount, Category.valueOf(category.toUpperCase()), date, description);
+    }
+
+
+
+    public  String handleAddExpense() {
+        Expense newExpense = readExpenseInput();
+        if( expenseService.addExpense(newExpense)) {
+            return "Expense added successfully";
+        } else {
+            return "Error adding expense";
+        }
+    }
+
+    public void handleViewExpenses() {
+        List<Expense> expenseList= expenseService.viewExpenses();
+        if(expenseList.isEmpty()) {
+            System.out.println("No expense added.");
+        }
+        else {
+            for(Expense e : expenseList) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    public void handleDeleteExpense() {
+        System.out.println("Enter the expense id to be deleted");
+        int deleteId = sc.nextInt();
+        sc.nextLine();
+        boolean isDeleted = expenseService.deleteExpense(deleteId);
+        if(isDeleted) {
+            System.out.println("Expense deleted successfully");
+        }
+        else {
+            System.out.println("Error deleting expense");
+        }
+    }
+
+    public void handleUpdateExpense() {
+        System.out.println("Enter id to update details");
+        int updateId = sc.nextInt();
+        sc.nextLine();
+        Expense updatedExpense = readExpenseInput();
+        boolean isUpdated = expenseService.updateExpense(updateId, updatedExpense);
+        if(isUpdated) {
+            System.out.println("Expenses updated successfully");
+        } else {
+            System.out.println("Error updating expense");
+        }
+    }
+
+    public void start() {
         System.out.println("==== Expense Tracker ====");
         boolean isExit = false;
         while(!isExit) {
@@ -25,85 +109,19 @@ public class ExpenseTrackerApp {
             System.out.println("Enter your choice");
 
             int choice = sc.nextInt();
+            sc.nextLine();
             switch (choice) {
                 case 1:
-                    System.out.println("\nEnter expense details");
-                    System.out.println("Enter Amount");
-                    double amount = sc.nextDouble();
-                    sc.nextLine();
-                    boolean isValidCategory = false;
-                    String category = null;
-                    while(!isValidCategory) {
-                        System.out.println("Select from Category");
-                        for (Category c: Category.values()) {
-                            System.out.println(c);
-                        }
-                        category=sc.nextLine();
-                        for (Category c: Category.values()) {
-                            if(c.name().equalsIgnoreCase(category)) {
-                                isValidCategory = true;
-                                break;
-                            }
-                        }
-                    }
-                    System.out.println("Enter Date(YYYY-MM-DD)");
-                    LocalDate date = LocalDate.parse(sc.nextLine());
-                    System.out.println("Enter Description");
-                    String description = sc.nextLine();
-                    Expense newExpense = new Expense(amount, Category.valueOf(category.toUpperCase()), date, description);
-                    String addMessage = expenseService.addExpense(newExpense);
-                    System.out.println(addMessage);
+                    System.out.println(handleAddExpense());
                     break;
                 case 2:
-                    List<Expense> expenseList= expenseService.viewExpenses();
-                    if(expenseList.isEmpty()) {
-                        System.out.println("No expense added.");
-                    }
-                    else {
-                        for(Expense e : expenseList) {
-                            System.out.println(e);
-                        }
-                    }
+                    handleViewExpenses();
                     break;
                 case 3:
-                    System.out.println("Enter the expense id to be deleted");
-                    int deleteId = sc.nextInt();
-                    String deleteMessage = expenseService.deleteExpense(deleteId);
-                    System.out.println(deleteMessage);
+                    handleDeleteExpense();
                     break;
                 case 4:
-                    System.out.println("Enter the expense id to be updated");
-                    int updateId = sc.nextInt();
-                    System.out.println("\nEnter the updatedexpense details");
-                    System.out.println("Enter Amount");
-                    double updatedAmount = sc.nextDouble();
-                    sc.nextLine();
-                    boolean isValidUpdatedCategory = false;
-                    String updatedCategory = null;
-                    while(!isValidUpdatedCategory) {
-                        System.out.println("Select from Category");
-                        for (Category c: Category.values()) {
-                            System.out.println(c);
-                        }
-                        updatedCategory=sc.nextLine();
-                        for (Category c: Category.values()) {
-                            if(c.name().equalsIgnoreCase(updatedCategory)) {
-                                isValidUpdatedCategory = true;
-                                break;
-                            }
-                        }
-                    }
-                    System.out.println("Enter Date(YYYY-MM-DD)");
-                    LocalDate updatedDate = LocalDate.parse(sc.nextLine());
-                    System.out.println("Enter Description");
-                    String updatedDescription = sc.nextLine();
-                    Expense updatedExpense = new Expense(updatedAmount, Category.valueOf(updatedCategory.toUpperCase()), updatedDate, updatedDescription);
-                    boolean isUpdated = expenseService.updateExpense(updateId, updatedExpense);
-                    if(isUpdated) {
-                        System.out.println("Expenses updated successfully");
-                    } else {
-                        System.out.println("Error updating expense");
-                    }
+                    handleUpdateExpense();
                     break;
                 case 5:
                     System.out.println("Exiting the app.");
@@ -114,5 +132,12 @@ public class ExpenseTrackerApp {
                     System.out.println("Invalid input");
             }
         }
+    }
+
+
+    public static void main(String[] args) {
+        ExpenseTrackerApp app = new ExpenseTrackerApp();
+        app.start();
+
     }
 }
--- src/main/java/com/expensetracker/service/ExpenseService.java
+++ src/main/java/com/expensetracker/service/ExpenseService.java
@@ -5,63 +5,48 @@ import com.expensetracker.model.Expense;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class ExpenseService {
-
-    public ExpenseService() {}
 
     private final List<Expense> expenseList = new ArrayList<>();
 
     private static int count = 1;
 
-    public String addExpense(Expense newExpense){
-        if(newExpense.getAmount() <=0) {
-            return "Amount should be greater than 0.";
-        }
-        if(newExpense.getDate().isAfter(LocalDate.now())) {
-            return "The date is in the future";
-        }
-        if(newExpense.getDescription() == null || newExpense.getDescription().isBlank()) {
-            return "Description should not be null or empty.";
-        }
-        Expense addExpense = new Expense(count++, newExpense.getAmount(), newExpense.getCategory(), newExpense.getDate(), newExpense.getDescription());
-        expenseList.add(addExpense);
-        return  "Expense added successfully";
+
+    private boolean isExpenseValid(Expense expense) {
+        if(expense == null) return false;
+       return expense.getAmount() >0
+               && expense.getDate() !=null
+               && !expense.getDate().isAfter(LocalDate.now())
+               && expense.getDescription() != null
+               && !expense.getDescription().isBlank();
     }
 
-    public ArrayList<Expense> viewExpenses() {
+    public boolean addExpense(Expense newExpense){
+        if(!isExpenseValid(newExpense)) return false;
+        Expense addExpense = new Expense(count++, newExpense.getAmount(), newExpense.getCategory(), newExpense.getDate(), newExpense.getDescription());
+        expenseList.add(addExpense);
+        return true;
+    }
+
+    public List<Expense> viewExpenses() {
         return new ArrayList<>(expenseList);
     }
 
-    public String deleteExpense(int id) {
+    public boolean deleteExpense(int id) {
         if(id <= 0) {
-            return "Id should be greater than 0.";
+            return false;
         }
         if(expenseList.isEmpty()) {
-            return "Expense list is empty nothing to be deleted.";
+            return false;
         }
-        boolean removed = expenseList.removeIf(row -> row.getId() == id);
-
-        if(!removed) {
-            return "Expense does not exist with id " + id;
-        } else {
-            return "Expense with id " + id + " deleted successfully";
-        }
+        return expenseList.removeIf(row -> row.getId() == id);
     }
 
     public boolean updateExpense(int id, Expense updatedExpense) {
-        if(updatedExpense.getAmount() <=0) {
-            return false;
-        }
-        if(updatedExpense.getDate().isAfter(LocalDate.now())) {
-            return false;
-        }
-        if(updatedExpense.getDescription() == null || updatedExpense.getDescription().isBlank()) {
-            return false;
-        }
+        if(!isExpenseValid(updatedExpense)) return false;
         Expense expenseSearch = expenseList.stream()
-                .filter(e -> Objects.equals(e.getId(), id))
+                .filter(e -> e.getId() == id)
                 .findFirst()
                 .orElse(null);
         if(expenseSearch == null) {
